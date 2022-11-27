da8f7d6e9ee5524bbc3f102f7e647ca2862fbee2
ryunalore <96705308+ryunalore@users.noreply.github.com>
2022-11-27 14:30:22 +0100

    new spring boot prj created

---
+++ src/main/java/fhtw/maintenance_monitor/MaintenanceMonitorApplication.java
@@ -0,0 +1,17 @@
+/**
+ * SLM Project:
+ * participants: Hannelore Fayefunmi, Nathalie Bistrak, Ilhan Kaplan
+ */
+package fhtw.maintenance_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMonitorApplication.class, args);
+    }
+
+}
