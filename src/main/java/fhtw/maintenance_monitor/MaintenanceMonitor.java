eba4ac711f99fcfb7bf478020082cc0d88788bc4
ryunalore <96705308+ryunalore@users.noreply.github.com>
2022-11-28 00:18:39 +0100

    Added: message, message/set, message/reset

--- src/main/java/fhtw/maintenance_monitor/MaintenanceMonitor.java
+++ src/main/java/fhtw/maintenance_monitor/MaintenanceMonitor.java
@@ -2,19 +2,39 @@ package fhtw.maintenance_monitor;
 
 public class MaintenanceMonitor {
 
+
+
+    private String message_current = "Everything works as expected";
+
     // constructor
     public MaintenanceMonitor(){
-
+        this.message_current = getMessage_current();
     }
 
+
+    public void setMessage_current(String message_current) {
+        this.message_current = message_current;
+    }
+
+    public String getMessage_current() {
+        return message_current;
+    }
+
+    public String message_set(String m) {
+        this.message_current = m.replace('+', ' ');
+        return "ok";
+    }
+
+    public String message_reset() {
+        this.message_current ="Everything works as expected";
+        return "ok";
+    }
+
+    /*
     // test code
     public long add(int numberOne, int numberTwo){
         return numberOne + numberTwo;
     }
-
-    public long message_standard () {
-        return 999;
-    }
-
+     */
 
 }
--- src/main/java/fhtw/maintenance_monitor/MaintenanceMonitorController.java
+++ src/main/java/fhtw/maintenance_monitor/MaintenanceMonitorController.java
@@ -1,9 +1,7 @@
 package fhtw.maintenance_monitor;
 
-
-import jakarta.ws.rs.POST;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +10,26 @@ public class MaintenanceMonitorController {
 
     private  MaintenanceMonitor monitor = new MaintenanceMonitor();
 
+    /*
+    // test code
     @GetMapping("/api/maintenancemonitor")
     public long addNumbers(@RequestParam(name="numberOne") int numberOne, @RequestParam(name="numberTwo") int numberTwo){
         return monitor.add(numberOne,numberTwo);
     }
+     */
 
-    @PostMapping("/api/message")
-    public long message(){
-        return monitor.message_standard();
+    @GetMapping("/api/message")
+    public String message(){
+        return monitor.getMessage_current();
     }
 
+    @PutMapping("/api/message/set")
+    public String message_set(@RequestParam(name="m") String m) {
+        return monitor.message_set(m);
+    }
+
+    @PutMapping("/api/message/reset")
+    public String message_reset() {
+        return monitor.message_reset();
+    }
 }
