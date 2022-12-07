29a8220531fc513cafd7ab7b6886274e662c6678
ryunalore <96705308+ryunalore@users.noreply.github.com>
2022-12-07 21:30:12 +0100

    minor changes, final version

--- src/main/java/fhtw/maintenance_monitor/MaintenanceMonitor.java
+++ src/main/java/fhtw/maintenance_monitor/MaintenanceMonitor.java
@@ -1,20 +1,22 @@
 package fhtw.maintenance_monitor;
 
+import org.springframework.data.annotation.Id;
+
 public class MaintenanceMonitor {
 
-
-
-    private String message_current = "Everything works as expected";
+    private String message_default = "Everything operates as expected";         // This is the default message, which can be defined here.
+    private String message_current = message_default;                           // Initiate with default message.
 
     // constructor
     public MaintenanceMonitor(){
         this.message_current = getMessage_current();
     }
 
-
+    /*
     public void setMessage_current(String message_current) {
         this.message_current = message_current;
     }
+     */
 
     public String getMessage_current() {
         return message_current;
@@ -22,19 +24,13 @@ public class MaintenanceMonitor {
 
     public String message_set(String m) {
         this.message_current = m.replace('+', ' ');
+        //this.message_current.replace('+',' ');
         return "ok";
     }
 
     public String message_reset() {
-        this.message_current ="Everything works as expected";
+        this.message_current = message_default;
         return "ok";
     }
-
-    /*
-    // test code
-    public long add(int numberOne, int numberTwo){
-        return numberOne + numberTwo;
-    }
-     */
 
 }
--- src/main/java/fhtw/maintenance_monitor/MaintenanceMonitorController.java
+++ src/main/java/fhtw/maintenance_monitor/MaintenanceMonitorController.java
@@ -1,6 +1,5 @@
 package fhtw.maintenance_monitor;
 
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,16 +7,9 @@ public class MaintenanceMonitorController {
 
     private  MaintenanceMonitor monitor = new MaintenanceMonitor();
 
-    /*
-    // test code
-    @GetMapping("/api/maintenancemonitor")
-    public long addNumbers(@RequestParam(name="numberOne") int numberOne, @RequestParam(name="numberTwo") int numberTwo){
-        return monitor.add(numberOne,numberTwo);
-    }
-     */
-
-    @GetMapping("/api/message")
+      @GetMapping("/api/message")
     public String message(){
+
         return monitor.getMessage_current();
     }
 
